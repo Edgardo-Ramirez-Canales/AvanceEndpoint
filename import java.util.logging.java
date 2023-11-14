5edd955058260bf31f36c44205b4327d782c1497
Edgardo-Ramirez-Canales <ramirezedgardo92@gmail.com>
2023-11-14 17:29:05 -0600

    martes d

--- import java.util.logging.java
+++ import java.util.logging.java
@@ -65,3 +65,107 @@ public String autenticar(String user, String pass) {
         }
     }
 ------------------------000000000000000000000----------------------------------
+2222222222222222222222222222222222222222222222222222
+
+package dao;
+
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import java.lang.reflect.Method;
+import java.util.List;
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.ext.Provider;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import java.util.Collections;
+
+//@Provider
+@Priority(Priorities.AUTHORIZATION)
+public class AuthorizationFilter implements ContainerRequestFilter {
+    
+    private TokenGenerator tokenGenerator = new TokenGenerator();
+
+    @Context
+    private ResourceInfo resourceInfo;
+    
+    public void filter(ContainerRequestContext requestContext) {
+        Method method = resourceInfo.getResourceMethod();
+        if (method.isAnnotationPresent(AuthorizationRequired.class)) {
+            // Obtener los roles de la anotación
+            String[] roles = method.getAnnotation(AuthorizationRequired.class).roles();
+
+            // Obtener el token del encabezado
+            String token = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+            if (token == null || !token.startsWith("Bearer ")) {
+                requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+                return;
+            }
+
+            token = token.substring(7); // Eliminar "Bearer "
+
+            // Verificar el token y los roles
+            if (!verifyTokenAndRoles(token, roles)) {
+                requestContext.abortWith(Response.status(Response.Status.FORBIDDEN).build());
+            }
+        }
+    }
+
+    private boolean verifyTokenAndRoles(String token, String[] requiredRoles) {
+        DecodedJWT decodedJWT = tokenGenerator.verifyToken(token);
+            if (decodedJWT == null) {
+                return false;
+            }
+            List<String> tokenRoles = getRolesFromToken(decodedJWT);
+            for (String requiredRole : requiredRoles) {
+                if (tokenRoles.contains(requiredRole)) {
+                return true;
+                }
+            }
+        return false;
+    }
+
+    private List<String> getRolesFromToken(DecodedJWT jwt) {
+        Claim rolesClaim = jwt.getClaim("roles"); // Asume que los roles están en un claim llamado "roles"
+        if (!rolesClaim.isNull()) {
+            return rolesClaim.asList(String.class);
+        }
+        return Collections.emptyList();
+    }
+    
+    
+    public static void main(String[] args) {
+        TokenGenerator tokenGenerator = new TokenGenerator();
+        AuthorizationFilter filter = new AuthorizationFilter();
+
+        String usuarioDePrueba = "edgardo";
+        String[] rolesDePrueba = {"admin", "usuario"};
+        String token = tokenGenerator.generateToken(usuarioDePrueba, rolesDePrueba);
+
+        // Asumiendo que el token contiene los roles "admin" y "usuario"
+        String[] requiredRoles = {"admin"};
+        boolean accesoPermitido = filter.verifyTokenAndRoles(token, requiredRoles);
+
+        System.out.println("Acceso Permitido: " + accesoPermitido);
+        
+        
+        DecodedJWT decodedJWT = tokenGenerator.verifyToken(token);
+            if (decodedJWT != null) {
+                List<String> roles = filter.getRolesFromToken(decodedJWT);
+                System.out.println("Roles en el token: " + roles);
+            } else {
+            System.out.println("El token no es válido.");
+        }
+    }
+    
+    
+
+}
+
+
+
+222222222222222222222222222222222222222222222222222222
